991f2b4d11a29deb93d9f4b8c1e16cb1aabe6387
Kai Matsuda <matsudaskai@gmail.com>
2018-05-29 03:15:28 -0400

    main nav mock up

--- client/src/Components/MainNav/index.tsx
+++ client/src/Components/MainNav/index.tsx
@@ -9,9 +9,17 @@ interface StateType {
 export default class MainNav extends React.Component<PropType, StateType> {
   render() {
     return (
-      <nav className='navbar navbar-dark bg-prm-color-dark'>
+      <nav className='navbar navbar-dark bg-primary'>
         <div className='container'>
-          <a className='navbar-brand' href='#'>LoL Stats</a>
+          <a className='navbar-brand text-uppercase' href='#'>LoL Stats</a>
+          <form className='form-inline'>
+            <div className='input-group'>
+              <input className='form-control text-muted' type='search' placeholder='Search' aria-label='Search'/>
+              <div className='input-group-append'>
+                <button className='btn btn-dark bg-primary-darken-2' type='submit'><span className="oi oi-magnifying-glass"></span></button>
+              </div>
+            </div>
+          </form>
         </div>
       </nav>
     )
